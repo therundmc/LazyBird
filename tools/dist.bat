c39dfb5bef50ad2faf0a8635c6b87a051e287011
Antoine CAVALLERA <antoine.cavallera@gmail.com>
2022-10-26 22:25:20 +0200

    fix tools

--- tools/dist.bat
+++ tools/dist.bat
@@ -10,6 +10,10 @@ copy .\libraries .\dist\libraries
 
 mkdir .\dist\assets
 copy .\assets .\dist\assets
+mkdir .\dist\assets\sound
+copy .\assets\sound .\dist\assets\sound
+mkdir .\dist\assets\img
+copy .\assets\img .\dist\assets\img
 
 mkdir .\dist\src
 copy .\src .\dist\src
