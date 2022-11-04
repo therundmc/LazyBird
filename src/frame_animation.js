7e3ee1b423e3619c41030231e96f262b7b0fdfa0
Antoine CAVALLERA <antoine.cavallera@gmail.com>
2022-11-04 18:15:03 +0100

    replace tab

--- src/frame_animation.js
+++ src/frame_animation.js
@@ -19,45 +19,45 @@
         this.sens = 1;
     }
     draw(x, y, scale) {
-		this.x = x;
-		this.y = y;
-		this.scale = scale;
+        this.x = x;
+        this.y = y;
+        this.scale = scale;
 
-		image(this.img, this.x, this.y, this.frame_width*this.scale, this.frame_height*this.scale,this.frame_width*this.animFrame+1,this.sourceY,this.frame_width,this.frame_height);
-		let now = new Date().getTime();
-			let delta = now - this.last;
-				if (delta >= 33) {
-					
-					this.last = now;
-					this.animFrame+=this.sens;
-				
-				if(this.animFrame < 0)
-				{
-				this.animFrame = 0;
-				this.sens*=(-1);
-				}
-				
-				if(this.animFrame > this.nbrOfFrames)
-				{
-				if(this.animationInv)
-				{
-					this.sens*=(-1);
-					this.animFrame = this.nbrOfFrames;
-				}
-				else
-					this.animFrame = 0;
-				}
-			}
+        image(this.img, this.x, this.y, this.frame_width*this.scale, this.frame_height*this.scale,this.frame_width*this.animFrame+1,this.sourceY,this.frame_width,this.frame_height);
+        let now = new Date().getTime();
+            let delta = now - this.last;
+                if (delta >= 33) {
+                    
+                    this.last = now;
+                    this.animFrame+=this.sens;
+                
+                if(this.animFrame < 0)
+                {
+                this.animFrame = 0;
+                this.sens*=(-1);
+                }
+                
+                if(this.animFrame > this.nbrOfFrames)
+                {
+                if(this.animationInv)
+                {
+                    this.sens*=(-1);
+                    this.animFrame = this.nbrOfFrames;
+                }
+                else
+                    this.animFrame = 0;
+                }
+            }
     } 
 
-	drawSpecFrame(frame) {
-		this.animFrame = frame;
-		image(this.img, this.x, this.y, this.frame_width*this.scale, this.frame_height*this.scale,this.frame_width*this.animFrame+1,this.sourceY,this.frame_width,this.frame_height);
-	}
+    drawSpecFrame(frame) {
+        this.animFrame = frame;
+        image(this.img, this.x, this.y, this.frame_width*this.scale, this.frame_height*this.scale,this.frame_width*this.animFrame+1,this.sourceY,this.frame_width,this.frame_height);
+    }
 
-	isDone() {
-		if (this.animFrame >= this.nbrOfFrames) {
-			return true
-		}
-	}   
+    isDone() {
+        if (this.animFrame >= this.nbrOfFrames) {
+            return true
+        }
+    }   
 }
