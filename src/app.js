07c5d99c66518c1220bb78fa87c46dbb91e2890f
Antoine CAVALLERA <antoine.cavallera@gmail.com>
2022-11-09 19:14:53 +0100

    music

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ function preload() {
   soundList[SOUND_LIST.BOOM] = loadSound('assets/sound/boom.wav');
   soundList[SOUND_LIST.ROBOTY] = loadSound('assets/sound/roboty.wav');
   soundList[SOUND_LIST.LAZYKAZE] = loadSound('assets/sound/lazykaze.mp3');
-  soundList[SOUND_LIST.LAZER_LONG] = loadSound('assets/sound/lazerLong.ogg');
+  soundList[SOUND_LIST.LAZER_LONG] = loadSound('assets/sound/lazerLong.mp3');
   soundList[SOUND_LIST.MISSILE] = loadSound('assets/sound/missile.wav');
 
   // Images
@@ -600,7 +600,7 @@ function handleSound() {
       break;
 
     case STATES.GAME_OVER:
-      playSound(soundList[SOUND_LIST.SONG], 0.7);
+      stopSound(soundList[SOUND_LIST.SONG], 0.7);
       break;
       
     default:
